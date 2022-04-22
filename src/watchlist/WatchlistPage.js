98ed92afcf047f8b8eb912ef464467939b38a43d
sinku <sininoir@gmail.com>
2022-04-23 00:55:26 +0430

    Add MoviesCapsule to Watchlist page

--- src/watchlist/WatchlistPage.js
+++ src/watchlist/WatchlistPage.js
@@ -23,6 +23,7 @@ import '../styles/watchlist.css';
 import React from 'react';
 import Navbar from '../components/Navbar'
 import WatchlistCapsule from './WatchlistCapsule';
+import MoviesCapsule from '../components/MoviesCapsule';
 
 class Watchlist extends React.Component {
     constructor(props) {
@@ -51,12 +52,36 @@ class Watchlist extends React.Component {
     }
 }
 
+class RecommendedMovies extends React.Component {
+    constructor(props) {
+        super(props); 
+        this.state = {items: [], DataisLoaded: false};
+    }
+
+    componentDidMount() {
+        fetch('http://127.0.0.1:8080/movies/')
+            .then(resp => resp.json())
+            .then(data => {
+                this.setState(prevState => ({items: data.data, DataisLoaded: true}));
+            });
+    }
+
+    render() {
+        var movies = this.state.items;
+    
+        return <MoviesCapsule movies={movies} title="پیشنهاد ها" />;
+    }
+}
+
 class WatchlistPage extends React.Component {
     render() {
         return (
             <div className="container">
                 <Navbar />
                 <Watchlist />
+                <div className="centered-container">
+                    <RecommendedMovies />
+                </div>
             </div>
         );
     }
