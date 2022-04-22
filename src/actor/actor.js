f5bec1bd0c93f1a9364128a6923de3a983d2dcbb
sina-negarandeh <negarandeh.sina@gmail.com>
2022-04-22 23:30:32 +0430

    Create actor.js

---
+++ src/actor/actor.js
@@ -0,0 +1,129 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import "../styles/actor.css";
+
+function CapsuleImage(props) {
+  return (
+    <div className="actor_profile_pic">
+      <img
+        src={props.actor.image}
+        alt={props.actor.name}
+        height="600"
+        width="400"
+      />
+    </div>
+  );
+}
+
+function ActorMovieCapsule(props) {
+  return (
+    <a class="recommended_movie rounded custome-text-white" href="">
+      <img alt={props.movie.name} src={props.movie.image} />
+      <div class="text">
+        {props.movie.name} <br />
+        {props.movie.imdbRate}
+      </div>
+    </a>
+  );
+}
+
+function ActorInformationCapsule(props) {
+  return (
+    <div className="information">
+      <h1 class="custome-text-white">مشخصات بازیگر</h1>
+      <div class="information_text custome-text-white">
+        <p>نام: {props.actor.name}</p>
+        <p>تاریخ تولد: {props.actor.birthDate}</p>
+        <p>ملیت: {props.actor.nationality}</p>
+        <p>تعداد فیلم‌ها: {props.movies.length}</p>
+      </div>
+      <div className="actor_movies">
+        <h2 class="custome-text-white">فیلم‌ها</h2>
+        <div class="recommended_movies rounded">
+          {props.movies.map(function (object, i) {
+            return <ActorMovieCapsule key={object.id} movie={object} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+class ActorInformation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], DataisLoaded: false };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:8080/movies/actors/2063")
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.setState((prevState) => ({
+          items: data.data,
+          DataisLoaded: true,
+        }));
+      });
+  }
+
+  render() {
+    return (
+      <div className="information">
+        <ActorInformationCapsule
+          actor={this.props.actor}
+          movies={this.state.items}
+        />
+      </div>
+    );
+  }
+}
+
+class ActorCapsule extends React.Component {
+  render() {
+    return (
+      <div className="actor_information">
+        <CapsuleImage actor={this.props.actor} />
+        <ActorInformation actor={this.props.actor} />
+      </div>
+    );
+  }
+}
+
+class Actor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], DataisLoaded: false };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:8080/actors/2063")
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.setState((prevState) => ({
+          items: data.data,
+          DataisLoaded: true,
+        }));
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <ActorCapsule actor={this.state.items} />
+      </div>
+    );
+  }
+}
+
+class ActorPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Actor />
+      </div>
+    );
+  }
+}
+
+export default ActorPage;
