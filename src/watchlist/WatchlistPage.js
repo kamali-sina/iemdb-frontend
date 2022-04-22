ddd7e60e346f67582c70ee61460adb175f99fdcc
sinku <sininoir@gmail.com>
2022-04-22 21:42:24 +0430

    Exctract WatchlistCapsule component

---
+++ src/watchlist/WatchlistPage.js
@@ -0,0 +1,65 @@
+// function GradesSection(props) {
+//     if (!props.isLoggedin)
+//         return (<h1>grades section</h1>);
+
+//     var coursesInTerms = Object.values(props.gradedCources).reduce(function(r, o){
+//         var k = o.term;   // unique `loc` key
+//         if (r[k] || (r[k]=[])) r[k].push({term:k, code: o.code, course: o.course, grade: o.grade});
+//         return r;
+//     }, {});
+
+//     return (
+//         <div className="col-lg-8 ">
+//             <section className="workbook_section">
+//                 {Object.values(coursesInTerms).map(termCourses => (
+//                     <TermGradesTable courses={termCourses}/>
+//                 ))}
+//             </section>
+//         </div>
+//     );
+// }
+
+import '../styles/watchlist.css';
+import React from 'react';
+import Navbar from '../components/Navbar'
+import WatchlistCapsule from './WatchlistCapsule';
+
+class Watchlist extends React.Component {
+    constructor(props) {
+        super(props); 
+        this.state = {items: [], DataisLoaded: false};
+    }
+
+    componentDidMount() {
+        fetch('http://127.0.0.1:8080/movies/')
+            .then(resp => resp.json())
+            .then(data => {
+                this.setState(prevState => ({items: data.data, DataisLoaded: true}));
+            });
+    }
+
+    render() {
+        var movies = this.state.items;
+    
+        return (
+            <div className="centered-container">
+                {movies.map(function(object, i){
+                    return <WatchlistCapsule key={object.id} movie={object} />; 
+                })}
+            </div>
+        );
+    }
+}
+
+class WatchlistPage extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <Navbar />
+                <Watchlist />
+            </div>
+        );
+    }
+}
+
+export default WatchlistPage;
