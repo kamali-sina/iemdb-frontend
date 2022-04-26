f9d924317fe8c7a6562f82b31d91b23ecd1374c6
sinku <sininoir@gmail.com>
2022-04-26 14:44:11 +0430

    Migrate to function components

--- src/movies/Movies.js
+++ src/movies/Movies.js
@@ -3,37 +3,34 @@ import React from 'react';
 import MoviesGrid from './MoviesGrid';
 import MoviesNavbar from './MoviesNavbar';
 
-class MoviesPage extends React.Component {
-    render() {
-        return (
-            <div className="page-container">
-                {/* TODO: Movies Navbar goes here */}
-                <MoviesNavbar />
-                
-                <div className="main-column-div">
-                    <div></div>
+function MoviesPage() {
+    return (
+        <div className="page-container">
+            {/* TODO: Movies Navbar goes here */}
+            <MoviesNavbar />
+            
+            <div className="main-column-div">
+                <div></div>
 
-                    <MoviesGrid />
+                <MoviesGrid />
 
+                {/* TODO: fix me please */}
+                <div className="sorting-culomn">
+                    <p className="sort-by-text">رتبه بندی بر اساس:</p>
+                    <div className="sort-by-div">
+                        <a href="" className="home-link">
+                            <p className="sort-by-items">تاریخ</p>
+                        </a>
 
-                    {/* TODO: fix me please */}
-                    <div className="sorting-culomn">
-                        <p className="sort-by-text">رتبه بندی بر اساس:</p>
-                        <div className="sort-by-div">
-                            <a href="" className="home-link">
-                                <p className="sort-by-items">تاریخ</p>
-                            </a>
-
-                            <a href="" className="home-link">
-                                <p className="sort-by-items">امتیاز Imdb</p>
-                            </a>
-                        </div>
+                        <a href="" className="home-link">
+                            <p className="sort-by-items">امتیاز Imdb</p>
+                        </a>
                     </div>
-
                 </div>
+
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default MoviesPage;
--- src/movies/MoviesNavbar.js
+++ src/movies/MoviesNavbar.js
@@ -1,53 +1,51 @@
 import React from 'react';
 // TODO: Fix this Component
-class MoviesNavbar extends React.Component {
-    render() {
-        return (
-            <header id="navbar">
-                <nav class="navbar">
-                    <a href="" class="home-link">
-                        <div class="logo-image">
-                            <img
-                                src={process.env.PUBLIC_URL + '/logo.png'}
-                                class="img-responsive img-rounded"
-                                alt="logo"
-                            />
-                        </div>
-                    </a>
-
-                    <form class="search-bar">
-                        <div class="dropdown">
-                            <button type="submit" class="dropbtn">
-                                جستجو بر اساس
-                                <i class="fa fa-sort-desc custome-drop-icon"></i>
-                            </button>
-
-                            <div class="dropdown-content">
-                                <a href="">نام</a>
-                                <a href="">ژانر</a>
-                                <a href="">تاریخ تولید</a>
-                            </div>
-                        </div>
-                        <input
-                        type="text"
-                        class="search-text-input custome-search-input"
+function MoviesNavbar() {
+    return (
+        <header id="navbar">
+            <nav class="navbar">
+                <a href="" class="home-link">
+                    <div class="logo-image">
+                        <img
+                            src={process.env.PUBLIC_URL + '/logo.png'}
+                            class="img-responsive img-rounded"
+                            alt="logo"
                         />
-                    </form>
+                    </div>
+                </a>
 
-                    <div class="dropdown-user">
-                        <div class="dropbtn-user">
-                            <i class="fa fa-user custome-user-icon"></i>
-                        </div>
+                <form class="search-bar">
+                    <div class="dropdown">
+                        <button type="submit" class="dropbtn">
+                            جستجو بر اساس
+                            <i class="fa fa-sort-desc custome-drop-icon"></i>
+                        </button>
 
-                        <div class="dropdown-content-user">
-                            <a href="/login">ثبت نام</a>
-                            <a href="">ورود</a>
+                        <div class="dropdown-content">
+                            <a href="">نام</a>
+                            <a href="">ژانر</a>
+                            <a href="">تاریخ تولید</a>
                         </div>
                     </div>
-                </nav>
-            </header>
-        );
-    }
+                    <input
+                    type="text"
+                    class="search-text-input custome-search-input"
+                    />
+                </form>
+
+                <div class="dropdown-user">
+                    <div class="dropbtn-user">
+                        <i class="fa fa-user custome-user-icon"></i>
+                    </div>
+
+                    <div class="dropdown-content-user">
+                        <a href="/login">ثبت نام</a>
+                        <a href="">ورود</a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
 }
 
 export default MoviesNavbar;
