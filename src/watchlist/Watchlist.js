fc715be44de3811cfeaca9cd29b3ee7d4ad98f51
sinku <sininoir@gmail.com>
2022-04-26 14:44:33 +0430

    Migrate to function components

--- src/watchlist/Watchlist.js
+++ src/watchlist/Watchlist.js
@@ -1,69 +1,65 @@
 import '../styles/watchlist.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar'
 import WatchlistCapsule from './WatchlistCapsule';
 import MoviesCapsule from '../components/MoviesCapsule';
 
-class Watchlist extends React.Component {
-    constructor(props) {
-        super(props); 
-        this.state = {items: [], DataisLoaded: false};
-    }
+function Watchlist() {
+    const [items, setItems] = useState([])
+    const [dataIsLoaded, setDataIsLoaded] = useState(false)
 
-    componentDidMount() {
+    function doFetch() {
         fetch('http://127.0.0.1:8080/movies/')
             .then(resp => resp.json())
             .then(data => {
-                this.setState(prevState => ({items: data.data, DataisLoaded: true}));
+                setItems(data.data);
+                setDataIsLoaded(true);
             });
     }
 
-    render() {
-        var movies = this.state.items;
-    
-        return (
+    useEffect(() => {
+        doFetch()
+    }, [items])
+
+    return (
+        <div className="centered-container">
+            {items.map(function(object, i){
+                return <WatchlistCapsule key={object.id} movie={object} />; 
+            })}
+        </div>
+    );
+}
+
+function RecommendedMovies() {
+    const [items, setItems] = useState([])
+    const [dataIsLoaded, setDataIsLoaded] = useState(false)
+
+    function doFetch() {
+        fetch('http://127.0.0.1:8080/movies/')
+            .then(resp => resp.json())
+            .then(data => {
+                setItems(data.data);
+                setDataIsLoaded(true);
+            });
+    }
+
+    useEffect(() => {
+        doFetch()
+    }, [items])
+
+    return <MoviesCapsule movies={items} title="پیشنهاد ها" />;
+}
+
+function WatchlistPage() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Watchlist />
             <div className="centered-container">
-                {movies.map(function(object, i){
-                    return <WatchlistCapsule key={object.id} movie={object} />; 
-                })}
+                <RecommendedMovies />
             </div>
-        );
-    }
-}
-
-class RecommendedMovies extends React.Component {
-    constructor(props) {
-        super(props); 
-        this.state = {items: [], DataisLoaded: false};
-    }
-
-    componentDidMount() {
-        fetch('http://127.0.0.1:8080/movies/')
-            .then(resp => resp.json())
-            .then(data => {
-                this.setState(prevState => ({items: data.data, DataisLoaded: true}));
-            });
-    }
-
-    render() {
-        var movies = this.state.items;
-    
-        return <MoviesCapsule movies={movies} title="پیشنهاد ها" />;
-    }
-}
-
-class WatchlistPage extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <Navbar />
-                <Watchlist />
-                <div className="centered-container">
-                    <RecommendedMovies />
-                </div>
-            </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default WatchlistPage;
