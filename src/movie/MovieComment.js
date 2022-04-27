8822987ac378ae93629d55ade86d740d66b686d6
sina-negarandeh <negarandeh.sina@gmail.com>
2022-04-27 20:57:12 +0430

    Fix clicking on vote would raise an error

--- src/movie/MovieComment.js
+++ src/movie/MovieComment.js
@@ -13,7 +13,7 @@ const Vote = ({ comment, notify }) => {
         },
         method: "POST",
         mode: "cors",
-        body: JSON.stringify({ vote: event.target.value }),
+        body: JSON.stringify({ vote: event.currentTarget.value }),
       }
     );
     const data = await response.json();
@@ -26,22 +26,14 @@ const Vote = ({ comment, notify }) => {
 
   return (
     <div className="icons">
-      <div className="thumbs-up">
-        <button className="fa fa-thumbs-up" value="1" onClick={voteComment}>
-          <div className="movie_text">{comment.numberOfLikes}</div>
-        </button>
-      </div>
-      <div className="thumbs-down">
-        <div className="thumbs-down">
-          <button
-            className="fa fa-thumbs-down"
-            value="-1"
-            onClick={voteComment}
-          >
-            <div className="movie_text">{comment.numberOfDislikes}</div>
-          </button>
-        </div>
-      </div>
+      <button className="thumbs-up" value="1" onClick={voteComment}>
+        <div className="fa fa-thumbs-up"></div>
+        <div className="movie_text">{comment.numberOfLikes}</div>
+      </button>
+      <button className="thumbs-down" value="-1" onClick={voteComment}>
+        <div className="fa fa-thumbs-down"></div>
+        <div className="movie_text">{comment.numberOfDislikes}</div>
+      </button>
     </div>
   );
 };
