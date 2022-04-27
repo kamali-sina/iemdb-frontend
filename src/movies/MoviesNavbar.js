68234bebf77c19955c615a2682b5dd3694657c19
sinku <sininoir@gmail.com>
2022-04-27 16:23:36 +0430

    Update MoviesNavbar

--- src/movies/MoviesNavbar.js
+++ src/movies/MoviesNavbar.js
@@ -1,48 +1,40 @@
 import React from 'react';
+import NavbarUserIcon from '../components/NavbarUserIcon';
+import NavbarLogo from '../components/NavbarLogo';
 // TODO: Fix this Component
+
+function SearchBar() {
+    return (
+        <form class="search-bar">
+            <div class="dropdown">
+                <button type="submit" class="dropbtn">
+                    جستجو بر اساس
+                    <i class="fa fa-sort-desc custome-drop-icon"></i>
+                </button>
+
+                <div class="dropdown-content">
+                    <a href="">نام</a>
+                    <a href="">ژانر</a>
+                    <a href="">تاریخ تولید</a>
+                </div>
+            </div>
+            <input
+            type="text"
+            class="search-text-input custome-search-input"
+            />
+        </form>
+    );
+}
+
 function MoviesNavbar() {
     return (
         <header id="navbar">
             <nav class="navbar">
-                <a href="" class="home-link">
-                    <div class="logo-image">
-                        <img
-                            src={process.env.PUBLIC_URL + '/logo.png'}
-                            class="img-responsive img-rounded"
-                            alt="logo"
-                        />
-                    </div>
-                </a>
+                <NavbarLogo />
 
-                <form class="search-bar">
-                    <div class="dropdown">
-                        <button type="submit" class="dropbtn">
-                            جستجو بر اساس
-                            <i class="fa fa-sort-desc custome-drop-icon"></i>
-                        </button>
+                <SearchBar />
 
-                        <div class="dropdown-content">
-                            <a href="">نام</a>
-                            <a href="">ژانر</a>
-                            <a href="">تاریخ تولید</a>
-                        </div>
-                    </div>
-                    <input
-                    type="text"
-                    class="search-text-input custome-search-input"
-                    />
-                </form>
-
-                <div class="dropdown-user">
-                    <div class="dropbtn-user">
-                        <i class="fa fa-user custome-user-icon"></i>
-                    </div>
-
-                    <div class="dropdown-content-user">
-                        <a href="/login">ثبت نام</a>
-                        <a href="">ورود</a>
-                    </div>
-                </div>
+                <NavbarUserIcon />
             </nav>
         </header>
     );
