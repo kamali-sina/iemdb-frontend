c5b274b9412168c5767cf1f03a8247153cec61a9
sinku <sininoir@gmail.com>
2022-04-27 16:23:55 +0430

    Abstract into more components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,14 @@
 import React from 'react';
+import NavbarLogo from './NavbarLogo';
+import NavbarUserIcon from './NavbarUserIcon';
 
 function Navbar() {
     return (
         <header id="navbar">
             <nav className="navbar">
-                {/* TODO: Add href */}
-                <a className="home-link" href="">
-                    <div className="logo-image">
-                        <img
-                                className="img-responsive img-rounded"
-                                src={process.env.PUBLIC_URL + '/logo.png'}
-                                alt="logo"
-                        />
-                    </div>
-                </a>
-                {/* TODO: Add href */}
-                <a className="home-link" href="">
-                    <div className="logo-image">
-                        <i className="fa fa-user custome-user-icon"></i>
-                    </div>
-                </a>
+                <NavbarLogo />
+
+                <NavbarUserIcon />                
             </nav>
         </header>
     );
