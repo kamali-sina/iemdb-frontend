4027a3357f56c1f84281fd125eb0205471a9c499
sinku <sininoir@gmail.com>
2022-05-22 10:12:03 +0430

    Add callback handling logic

--- src/users/Callback.js
+++ src/users/Callback.js
@@ -8,6 +8,26 @@ function CallbackPage({notify}) {
     const [isUserLoggedIn, setIsUserLoggedIn] = useState(null)
     const [searchParams, setSearchParams] = useSearchParams();
 
+    async function doFetch(code) {
+        const response = await fetch('http://127.0.0.1:8080/callback/', { 
+            headers: {'Content-Type': 'application/json', 
+                      'Accept': 'application/json'},
+            method: 'POST', 
+            mode: 'cors', 
+            redirect: 'follow',
+            body: JSON.stringify({ "code": code })       
+        });
+        const data = await response.json();
+        console.log('tokes: ' + data.status + ': ' + data.data);
+        if (data.status == 200) {
+            localStorage.setItem('token', data.data)
+            navigate("/")
+        } else {
+            notify("login using github failed!!")
+            navigate("/login")
+        }
+    }
+
     useEffect(() => {
         if (localStorage.getItem('token') === 'null') {
             setIsUserLoggedIn(false)
@@ -20,7 +40,7 @@ function CallbackPage({notify}) {
             notify("There was a problem!")
             navigate('/login')
         }
-        console.log("code is: " + auth_code)
+        doFetch(auth_code)
     }, [isUserLoggedIn])
 
     return (
